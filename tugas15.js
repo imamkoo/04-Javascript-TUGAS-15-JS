581a11ef03683afcbad242593834938485ec4812
Imamkoo <imammm.kooo@gmail.com>
2021-12-16 16:52:22 +0700

    Tugas 15 Javascript

---
+++ tugas15.js
@@ -0,0 +1,6 @@
+let ab = 8;
+let bc = 6;
+
+console.log("Nilai AB : " + ab);
+console.log("Nilai BC : " + bc);
+console.log("Panjang sisi AC pada segitiga siku-siku tersebut adalah : " + Math.sqrt(Math.pow(ab, 2) + Math.pow(bc, 2)));
